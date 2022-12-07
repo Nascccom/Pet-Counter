6291e2b554f94df988ab3199d69a24bd1328baff
Nascccom <ostrov_o@bk.ru>
2022-12-07 13:54:56 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {CounterDisplay} from './Components/CounterDisplay';
+
+function App() {
+    return (
+      <div className="App">
+          <CounterDisplay/>
+      </div>
+    );
+}
+
+export default App;
